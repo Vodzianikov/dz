a9f6bd9d02b172f34e30ac8d4d58f5ffcb174a9d
Vodzianikov <125147632+Vodzianikov@users.noreply.github.com>
2023-06-29 16:49:59 +0300

    Add files via upload

---
+++ dz-object-class2/script.js
@@ -0,0 +1,30 @@
+//3) Создать класс Programmer который создает объекты с полями: name, age, tech
+// (массив)
+// Создать массив из 10 программеров
+// Отфильтровать массив так, чтобы в нем находились только программеры старше 30 и
+// со стеком больше 3
+
+
+
+// class Programs {
+//     constructor(name,age,tech){
+//         const mass = [this.name = name,this.age = age,this.tech = tech]
+//             if(this.age < 30){
+//                 const massSplice = mass.splice(0) 
+//             }
+//             if(this.tech < 3){
+//                 const massSplice = mass.splice(0)
+//             }
+//             console.log(...mass)
+// }
+// }
+// const programmer = new Programs("Максим",30,4)
+// const programmer1 = new Programs("Ира",29,5)
+// const programmer3 = new Programs("Ярослав",36,16)
+// const programmer4 = new Programs("Никита",69,4)
+// const programmer5 = new Programs("Руслан",10,1)
+// const programmer6 = new Programs("Савелий",31,3)
+// const programmer7 = new Programs("Анастасия",30,4)
+// const programmer8 = new Programs("Захар",32,7)
+// const programmer9 = new Programs("Ержан",1000,1000)
+// const programmer10 = new Programs("Ярослав",25,5)
